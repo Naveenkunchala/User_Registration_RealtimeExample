1a54f7adff6451c54607de62bacdd129bbfbb08c
kunchalanaveen <kunchalanaveen80@gmail.com>
2023-05-08 11:34:47 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,11 +3,21 @@ from django.http import HttpResponse
 
 from django.core.mail import send_mail
 
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+
+
 # Create your views here.
 from app.forms import *
 
 
 def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
     return render(request,'home.html')
 
 def Registration(request):
@@ -41,3 +51,25 @@ def Registration(request):
             return HttpResponse('Not valid')
 
     return render(request,'registration.html',d)
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid username or password') 
+        
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+    
